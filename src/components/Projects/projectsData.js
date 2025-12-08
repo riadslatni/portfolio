2f2251428b5d5a3a26dd08262c56584a8ca2e946
riadslatni <137224610+riadslatni@users.noreply.github.com>
2025-12-08 18:53:08 +0100

    Fix images for GitHub Pages

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -2,35 +2,31 @@ const Projects = [
     {
         title: "Premier pas sur le language HTML",
         description: "Ce projet était une introduction aux fondamentaux du langage HTML. Nous avons travailler sur la mise à jour de la page web de l'association Riding cities, conformément aux maquettes fournies.",
-        image: "/assets/ridingcities.webp",
+        image: "/portfolio/assets/ridingcities.webp",
         alt: "Skate rouge",
         tools: ["HTML", "CSS", "Figma", "VS Code"],
         github: "https://github.com/riadslatni/site-Riding-Citites.git"
-
     },
     {
         title: "Créez la page d'accueil d'une agence de voyage avec HTML & CSS",
         description: "Création de la page d'accueil d'une agence de voyage en utilisant HTML et CSS, la mission principale était d'intégrer l'interface responsive de leur site Booki.",
-        image: "/assets/booki.webp",
+        image: "/portfolio/assets/booki.webp",
         alt: "Site d'une agence de voyage",
         tools: ["HTML", "CSS", "Figma"],
         github: "https://github.com/riadslatni/Booki.git"
-
     },
-
     {
         title: "Améliorez l'interface d'un site mobile avec des animations CSS",
         description: "Intégration d'une maquette en mobile-first, utilisation de Sass, implémentation d'animations CSS.",
-        image: "/assets/ohmyfood.webp",
+        image: "/portfolio/assets/ohmyfood.webp",
         alt: "Menu d'un restaurant",
         tools: ["HTML", "CSS", "Figma", "Sass", "Github"],
         github: "https://github.com/riadslatni/projetohmyfood.git"
-
     },
     {
         title: "Premier pas sur le language Javascript",
         description: "Réalisez le carrousel du site Internet d’une imprimerie avec JavaScript.",
-        image: "/assets/printit.webp",
+        image: "/portfolio/assets/printit.webp",
         alt: "Homme tenant un journal",
         tools: ["HTML", "CSS", "Javascript", "Github"],
         github: "https://github.com/riadslatni/Print-it-JS.git"
@@ -38,16 +34,15 @@ const Projects = [
     {
         title: "Créez une page web dynamique avec Javascript",
         description: "Créez un site dynamique avec JavaScript, communiquant avec une API pour une architecte.",
-        image: "/assets/sophiebluel.webp",
+        image: "/portfolio/assets/sophiebluel.webp",
         alt: "Photo d'une jeune femme ",
         tools: ["Javascript", "HTML", "CSS", "Swagger", "Github"],
         github: "https://github.com/riadslatni/sophie-bluel-.git"
-
     },
     {
         title: "Plannifiez le développement du site de votre client",
         description: "Mise en place d'une veille technologique, rédaction des spécifications techniques du projet, et d'un tableau kanban.",
-        image: "/assets/qwenta.webp",
+        image: "/portfolio/assets/qwenta.webp",
         alt: "Une coupe de glace ",
         tools: ["React", "Javascript", "Notion", "Figma", "Feedly", "Agile", "Scrum", "GitHub"],
         github: "https://github.com/riadslatni/qwenta.git"
@@ -55,38 +50,35 @@ const Projects = [
     {
         title: "Créez une application web de location immobilière avec React",
         description: "Implémenter le front-end d’une application en utilisant React et React Router pour créer une expérience utilisateur moderne et réactive.",
-        image: "/assets/kasa.webp",
+        image: "/portfolio/assets/kasa.webp",
         alt: "Appartement joliement décoré",
         tools: ["React", "React rooter", "Javascript", "HTML", "CSS", "Github"],
         github: "https://github.com/riadslatni/kasa.git"
-
     },
     {
-        title: "Optimisez le référencement d'un site de photographe ",
+        title: "Optimisez le référencement d'un site de photographe",
         description: "Identifier les problèmes de chargement et de référencement, pour ensuite améliorer les performances et l'accessibilité du site d'une photographe puis de réaliser un rapport d'optimisation.",
-        image: "/assets/ninacarducci.webp",
+        image: "/portfolio/assets/ninacarducci.webp",
         alt: "Photo de nina carducci",
         tools: ["HTML", "CSS", "Lighthouse", "Wave", "Google Rich Snippet", "Github"],
         github: "https://github.com/riadslatni/ninacarduccip9.git"
-
     },
-
     {
         title: "Débuggez le site d'une agence d'événementiel",
         description: "Identifier et comprendre les problèmes et bugs signalés à l'aide des Chrome DevTools.",
-        image: "/assets/724events.webp",
+        image: "/portfolio/assets/724events.webp",
         alt: "Un groupe de personnes en événement",
         tools: ["React", "React Developper Tools", "Javascript", "Nodejs", "Yarn", "GitHub"],
         github: "https://github.com/riadslatni/724events.git"
     },
-
     {
         title: "Implémentez le front-end d'une application bancaire avec React",
         description: "Intégrer le front-end avec le back-end via des appels API.",
-        image: "/assets/argentbank.webp",
+        image: "/portfolio/assets/argentbank.webp",
         alt: "Logo ArgentBank, une banque en ligne",
         tools: ["React", "Javascript", "Redux", "HTML", "CSS", "MongoDb", "NodeJS", "API REST", "Swagger", "Postmann", "GitHub"],
         github: "https://github.com/riadslatni/ARGENTBANK.git"
-    },
+    }
 ];
+
 export default Projects;
